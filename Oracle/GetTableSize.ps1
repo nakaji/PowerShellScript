9940f0ab62314895412a22f69c2827746f56acdc
nakaji <nakaji126@gmail.com>
2012-10-25 17:29:14 +0900

    テーブルのサイズとデータ件数を取得するスクリプトを追加

---
+++ Oracle/GetTableSize.ps1
@@ -0,0 +1,31 @@
+[void][reflection.assembly]::LoadWithPartialName("Oracle.DataAccess")
+
+$conStr = "Data Source=XE;User Id=hr;Password=hr"
+
+$con = New-Object Oracle.DataAccess.Client.OracleConnection($conStr)
+$con.Open()
+
+$sqlStr="select segment_name,segment_type,sum(bytes)/1024/1024 as MB 
+from user_extents 
+group by segment_name,segment_type
+order by segment_type,segment_name"
+
+$cmd = New-Object Oracle.DataAccess.Client.OracleCommand($sqlStr, $con)
+$reader = $cmd.ExecuteReader()
+
+Write-Host ("  {0, -10} {1, -30} {2, 10} {3, 15}" -F "TYPE", "OBJECT_NAME" ,"SIZE(MB)", "RECORD_COUNT")
+while ( $reader.read() )
+{
+    if ($reader[1] -eq "TABLE"){
+        $countSql=("select count(*) from {0}" -F $reader[0])
+        $cmd = New-Object Oracle.DataAccess.Client.OracleCommand($countSql, $con)
+        $countReader = $cmd.ExecuteReader()
+        [void]$countReader.read()
+        
+        Write-Host ("  {0, -10} {1, -30} {2, 10} {3, 15}" -F $reader[1] ,$reader[0] ,$reader[2], $countReader[0])
+    }else{
+        Write-Host ("  {0, -10} {1, -30} {2, 10}" -F $reader[1] ,$reader[0] ,$reader[2])
+    }
+
+}
+$con.Close()
