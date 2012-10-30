802946facdb296d1cd190bd62f87121000bb0718
nakaji <nakaji126@gmail.com>
2012-10-30 12:11:35 +0900

    書式の指定を1か所にまとめた

--- Oracle/GetTableSize.ps1
+++ Oracle/GetTableSize.ps1
@@ -13,7 +13,8 @@ order by segment_type,segment_name"
 $cmd = New-Object Oracle.DataAccess.Client.OracleCommand($sqlStr, $con)
 $reader = $cmd.ExecuteReader()
 
-Write-Host ("  {0, -10} {1, -30} {2, 10} {3, 15}" -F "TYPE", "OBJECT_NAME" ,"SIZE(MB)", "RECORD_COUNT")
+$outputDataFormat="  {0, -10} {1, -30} {2, 10} {3, 15}"
+Write-Host ($outputDataFormat -F "TYPE", "OBJECT_NAME" ,"SIZE(MB)", "RECORD_COUNT")
 while ( $reader.read() )
 {
     if ($reader[1] -eq "TABLE"){
@@ -22,9 +23,9 @@ while ( $reader.read() )
         $countReader = $cmd.ExecuteReader()
         [void]$countReader.read()
         
-        Write-Host ("  {0, -10} {1, -30} {2, 10} {3, 15}" -F $reader[1] ,$reader[0] ,$reader[2], $countReader[0])
+        Write-Host ($outputDataFormat -F $reader[1] ,$reader[0] ,$reader[2], $countReader[0])
     }else{
-        Write-Host ("  {0, -10} {1, -30} {2, 10}" -F $reader[1] ,$reader[0] ,$reader[2])
+        Write-Host ($outputDataFormat -F $reader[1] ,$reader[0] ,$reader[2], "")
     }
 
 }
