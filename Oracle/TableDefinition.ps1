012511a40236ec9088284c3d78e153c0fee7b398
nakaji <nakaji126@gmail.com>
2014-02-06 16:04:35 +0900

    テーブルの定義（descの結果）を出力するスクリプトを追加

---
+++ Oracle/TableDefinition.ps1
@@ -0,0 +1,40 @@
+﻿[System.Reflection.Assembly]::LoadWithPartialName("Oracle.DataAccess") | Out-Null
+cls
+
+function GetUserTables {
+param($uid, $pass, $svc)
+    $connstr = "User ID=$uid;Password=$pass;Data Source=$svc"
+    $conn = New-Object Oracle.DataAccess.Client.OracleConnection($connstr)
+
+    $conn.Open()
+    $cmd = New-Object Oracle.DataAccess.Client.OracleCommand
+
+    $cmd.Connection = $conn
+    $cmd.CommandText = "select TABLE_NAME from USER_TABLES order by TABLE_NAME"
+
+    $reader = $cmd.ExecuteReader()
+    while ($reader.Read())
+    {
+        $reader[0].ToString()
+    }
+}
+
+function OutputDefiniton {
+param($uid, $pass, $svc, $talbes)
+
+    if (!(Test-Path $uid)) { mkdir $uid | Out-Null }
+    $talbes | %{
+        $tableName=$_
+        "desc $tableName" | sqlplus -S $uid/$pass@$svc | Out-File "$uid\$tableName.def" -Encoding default
+    }
+}
+
+$uid = Read-Host "UserID:"
+$pass = Read-Host "Password:"
+$svc = Read-Host "ServiceName:"
+
+$talbes = GetUserTables $uid $pass $svc
+
+OutputDefiniton $uid $pass $svc $talbes
+
+trap{ exit }
